af0a93df0fd4b76866e199ee553a3bf2f43fb24a
Александр Сысоев <alexgood5000@gmail.com>
2025-09-24 11:11:33 +0300

    Реализовал функционал добавления и удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,21 @@
-// @todo: Темплейт карточки
+const createCard = (cardData, deleteCardHandler) => {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
-// @todo: DOM узлы
+  cardElement.querySelector('.card__title').textContent = cardData.name;
+  cardElement.querySelector('.card__image').src = cardData.link;
 
-// @todo: Функция создания карточки
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCardHandler);
 
-// @todo: Функция удаления карточки
+  return cardElement;
+};
 
-// @todo: Вывести карточки на страницу
+const deleteCard = (e) => {
+  e.target.parentElement.remove();
+}
+
+const cardsListElement = document.querySelector('.places__list');
+
+initialCards.forEach((card) => {
+  cardsListElement.append(createCard(card, deleteCard));
+});
